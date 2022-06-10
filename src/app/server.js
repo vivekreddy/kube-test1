3f1286a73b04f07af90f5cdd8974597ffc5746c5
Vivek Vardhan Reddy <vivekvardhan.s@gmail.com>
2022-06-11 01:58:25 +0530

    Update server.js

--- src/app/server.js
+++ src/app/server.js
@@ -16,7 +16,8 @@ app.set('view engine', 'handlebars');
 // Configuration
 
 var port = process.env.PORT || 8080;
-var message = process.env.MESSAGE || 'Hello Vivek Reddy!';
+//var message = process.env.MESSAGE || 'Hello Vivek Reddy!';
+var message = 'Hello Vivek Reddy!';
 var renderPathPrefix = (
   process.env.RENDER_PATH_PREFIX ? 
     '/' + process.env.RENDER_PATH_PREFIX.replace(/^[\\/]+/, '').replace(/[\\/]+$/, '') :
